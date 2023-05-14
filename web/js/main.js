b401f0b281da261c98585115f8a6ef4b4eba5a01
minusone98 <minusone98.respaldo@gmail.com>
2023-05-14 10:52:01 -0300

    someChanges

--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,24 @@
+const slider = document.querySelector('.slider');
+let scrollAmount = 0;
+const scrollStep = 1;
 
+function slideRight() {
+  scrollAmount += slider.offsetWidth;
+  slider.scroll({
+    left: scrollAmount,
+    behavior: 'smooth'
+  });
+}
+
+function slideLeft() {
+  scrollAmount -= slider.offsetWidth / 2;
+  slider.scroll({
+    left: scrollAmount,
+    behavior: 'smooth'
+  });
+}
+
+document.querySelector('.prev-button').addEventListener('click', slideLeft);
+document.querySelector('.next-button').addEventListener('click', slideRight);
+
+setInterval(slideRight, 5000);
