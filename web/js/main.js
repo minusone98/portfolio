1611078eac4b4fcee3e2be38c5eaa9ee72a6d0f0
minusone98 <minusone98.respaldo@gmail.com>
2023-05-15 12:04:18 -0300

    NewForm

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,5 @@
+// SLIDER CODE
+
 const   slider            = document.querySelector('#slider');
 let     sliderSection     = document.querySelectorAll('.slider__section');
 let     sliderSectionLast     = sliderSection[sliderSection.length-1];
